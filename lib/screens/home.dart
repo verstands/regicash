88dfd9174ac58273c8b102cee72283c457e3725e
Rabby <rabbymbamu@gmail.com>
2022-05-13 19:03:35 +0200

    effacer_tout_le_code

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,27 +11,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("sample App"),
-        ),
-        body: Center(
-            child: Column(
-              children: [
-                  Column(
-                      children:const [
-                        SizedBox(height: 20,),
-                         Center(
-                           child : Text("RegiCash", style: TextStyle(color: Colors.blue, fontSize: 40),)
-                         ),
-                         SizedBox(height: 20,),
-                         Center(
-                           child: Text("Sign in", style: TextStyle(fontSize: 20),) ,
-                         ), 
-                      ],
-                ),
-              ],
-            ),
-        ),
+        body: Container(
+           child: const Image(image: AssetImage('assets/images/equity-bank-logo.png'),),
+        ) 
     );
   }
 }
